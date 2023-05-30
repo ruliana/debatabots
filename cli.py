8e4e07b90c0f9f6dfa9a06b9e1de8641c611e84c
Ronie Uliana <ronie.uliana@gmail.com>
2023-05-30 23:26:44 +0200

    Add the Socratic Philospher as opponent

--- cli.py
+++ cli.py
@@ -64,21 +64,48 @@ class Debate(object):
             Think step by step and dismatle the functional programming evangelist's arguments using your knowledge of programming and their challenges.
             Always use bullet points to enumerate your arguments. IMPORTANT: Do not summarize or conclude your arguments, just enumerate them.
             Some benefits, like "easy to understand", are personal and subjective, so you can't use them as arguments.
-            Provide examples to illustrate your counter-arguments.
+            IMPORTANT: Make sure to add examples to illustrate your points.
             ''',
                  name='OOP Evangelist',
                  history=[{'role': 'assistant', 'content': 'OOP is better than functional programming for code reuse.'}],
             ),
             Chat('''
-            You are a Functional Programming Evangelist.
-            You are debating with a OOP evangelist about code reuse.
-            Your goal is to find which situations are better for functional programming for code reuse.
-            Think step by step and dismatle the OOP evangelist's arguments using your knowledge of programming and their challenges.
-            Always use bullet points to enumerate your arguments. IMPORTANT: Do not summarize or conclude your arguments, just enumerate them.
-            Some benefits, like "easy to understand", are personal and subjective, so you can't use them as arguments.
-            Provide examples to illustrate your counter-arguments.
+            You are a Socratic Philosopher.
+            You are debating with a OOP Evangelist about code reuse and your goal is to estimulate their critical thinking by questioning their beliefs.
+            You are not trying to win the debate, but to make them find the root point of each claim.
+            Use the Socratic Questioning to do this. You can use the following questions:
+
+            1. **Clarifying concepts:**
+                - Can you elaborate on what you mean by that point?
+                - How would you define this concept in your own words?
+                - Can you provide a concrete example to explain this?
+
+            2. **Probing assumptions:**
+                - What are you assuming when you say that?
+                - Why do you think that assumption holds true?
+                - Can we think of a situation where that assumption may not be valid?
+
+            3. **Probing reasons and evidence:**
+                - What reasons do you have for believing that?
+                - Can you provide evidence to support your point of view?
+                - How do you reconcile this idea with [contrary evidence]?
+
+            4. **Questioning viewpoints and perspectives:**
+                - Have you considered any alternative viewpoints?
+                - How might [different person or group] see this issue?
+                - Why do you think others might disagree with you?
+
+            5. **Probing implications and consequences:**
+                - If we follow your argument to its logical conclusion, what would happen?
+                - What are the potential implications of your viewpoint?
+                - What might be the consequences if we ignore this issue?
+
+            6. **Questioning the question:**
+                - Why do you think I asked this question?
+                - What does this question assume?
+                - Is there another way to phrase the question that might provide a different perspective or answer?
             ''',
-                 name='Functional Programming Evangelist',
+                 name='Socratic Philosopher',
             ),
         ]
         # We count the number of "Thank you", "You're welcome", and such to determine when the debate is over.
@@ -108,7 +135,7 @@ class Debate(object):
 
         # Check if the debate is over
         # Yes, in every instance of the debate, it ended with them thanking each other.
-        if re.search(r'\b(thank you|you\'re welcome|great day)\b', last_message, re.IGNORECASE):
+        if re.search(r'\b(thank you|you\'re welcome|great day|i completely agree)\b', last_message, re.IGNORECASE):
             self.thank_you_count -= 1
 
         if self.thank_you_count == 0:
@@ -121,7 +148,7 @@ class Debate(object):
         """End the debate with a summary of the discussion."""
         print(f'=== End of debate! ===')
         print(self.current_chat().get_response('''
-        The debate is over. Summarize it, enumarating the pros and cons of each side.
+        The debate is over. Let's summarize the discussion. Highlight the takeaways of the debate and the points that were not addressed.
         '''))
         sys.exit(0)
 
@@ -132,13 +159,13 @@ class Debate(object):
         """
         print('=== Provocation ===')
         self.print_last_message()
-        for turn in range(self.turn_count, 0, -1):
+        for turn in range(self.turn_count):
             print()
-            print(f'=== Turn {turn:02} ===')
+            print(f'=== Turn {turn + 1:02} ===')
             self.turn()
             time.sleep(3)
 
-        end_debate()
+        self.end_debate()
 
 if __name__ == '__main__':
     Debate().loop()
